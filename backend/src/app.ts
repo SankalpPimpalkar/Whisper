4371249d3c4a4679d6d98c8802d46e344d8954f9
SankalpPimpalkar <pimpalkarsankalp8@gmail.com>
2026-01-24 21:16:36 +0530

    Added Auth Routes and Controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,12 @@ import authRoutes from "./routes/authRoutes";
 import chatRoutes from "./routes/chatRoutes";
 import messageRoutes from "./routes/messageRoutes";
 import userRoutes from "./routes/userRoutes";
+import { clerkMiddleware } from "@clerk/express";
+import errorHandler from "./middlewares/errorHandler";
 const app = express()
 
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get('/', (req, res) => {
     return res.send("Hello World")
@@ -15,5 +18,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/chats', chatRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
 
 export default app
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import protectRoute from "../middlewares/auth";
+import { authCallback, getMe } from "../controllers/authController";
 
 const authRoutes = Router()
 
+authRoutes.get('/me', protectRoute, getMe)
+authRoutes.get('/callback', authCallback)
 
 export default authRoutes
